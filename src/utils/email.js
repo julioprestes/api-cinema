cf6ef4b67141f07b873cd90a63708219e80efb20
julio <juliocmp@unochapeco.edu.br>
2025-04-29 16:15:20 -0300

    update email

--- src/utils/email.js
+++ src/utils/email.js
@@ -7,7 +7,7 @@ async function sendMail(to, name, body, subject) {
         secure: false, 
         auth: {
             user: 'juliocmp@unochapeco.edu.br',
-            pass: 'lwdc qokf fgqp lrpt ',
+            pass: process.env.SMTP_KEY,
         }
     });
 
@@ -21,14 +21,3 @@ async function sendMail(to, name, body, subject) {
 }
 
 export default sendMail
-
-//criar uma chave de acesso global do google
-
-//fazer um recuperar minha senha (receber um email com um codigo temporario)
-// criar coluna no banco para codigo temoprario (na tabela usuario), depois envio de email para o email
-//  do usuario (utilizando a funcao (usando import)) rota para receber codigo, rota para atualizar senha
-// token com expirein(30m) || ou criar coluna no banco (timestamp) que qauando craido o codigo pegue a hora atual 
-// + tempo de expiração 
-
-//CRIAR UMA COLUNA DO BANCO PARA CODIO TEMPORARIO, ENVIO EMAIL PARA O EMAIL DO  USUARIO UTILIZANDO A FUNCAO, ROTA PARA RECEBER CODIGO,
-//  ROTA PARA ATUALIZAR SENHA -> TOKEN EXPIREIN 30M || CRIAR COLUNA NO BANCO PREFERENCIALMENTE TIMESTAMP QUE QUANDO CRIADO O CODIGO PEGUE A HORA ATUAL + TEMPO DE EXPIRAÇÃO
